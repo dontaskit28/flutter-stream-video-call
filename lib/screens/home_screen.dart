a2307a0e99a55b68f0a2892b4020a7942ea8ee6f
dontaskit28 <jagutatarao28@gmail.com>
2024-06-23 12:04:44 +0530

    added api call to get tokens from backend

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_stream_video_call/screens/call_screen.dart';
 import 'package:flutter_stream_video_call/screens/loader_dialog.dart';
 import 'package:flutter_stream_video_call/utils/constansts.dart';
 import 'package:stream_video_flutter/stream_video_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,16 +15,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
   @override
   void initState() {
+    _nameController.addListener(() {
+      setState(() {});
+    });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
   }
 
   makeACall({
     required String userId,
-    required String userToken,
+    // required String userToken,
     required String name,
-    required String role,
+    String role = 'admin',
     required BuildContext context,
   }) async {
     try {
@@ -32,7 +46,14 @@ class _HomeScreenState extends State<HomeScreen> {
           name: name,
           role: role,
         ),
-        userToken: userToken,
+        // userToken: userToken,
+        tokenLoader: (userId) async {
+          print("Testing....... $userId");
+          return await getUserToken(
+            userId: userId,
+            name: name,
+          );
+        },
         options: const StreamVideoOptions(
           logPriority: Priority.debug,
         ),
@@ -66,38 +87,69 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  Future<String> getUserToken({
+    required String userId,
+    required String name,
+    String role = 'admin',
+  }) async {
+    final url = Uri.parse('$apiUrl/createUser');
+    Map<String, dynamic> body = {
+      'userId': userId,
+      'name': name,
+      'role': role,
+    };
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+    };
+    debugPrint('Request body: $body');
+    final response = await http.post(
+      headers: headers,
+      url,
+      body: json.encode(body),
+    );
+    print("Testing....... ${response}");
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['token'];
+    } else {
+      debugPrint('Testing......: ${response.body}');
+      throw Exception('Failed to get user token');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Center(
-          child: ListView.separated(
-            separatorBuilder: (context, index) => const SizedBox(height: 20),
-            itemCount: users.length,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              return ElevatedButton(
-                child: Text(
-                  "Join as ${users[index].name}",
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter your name',
                 ),
-                onPressed: () async {
-                  showLoaderDialog(context);
-                  await makeACall(
-                    userId: users[index].userId,
-                    userToken: users[index].userToken,
-                    name: users[index].name,
-                    role: users[index].role,
-                    context: context,
-                  );
-                },
-              );
-            },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _nameController.text.isEmpty
+                    ? null
+                    : () async {
+                        showLoaderDialog(context);
+                        String userId =
+                            '${_nameController.text}-${DateTime.now().millisecondsSinceEpoch}';
+                        if (context.mounted) {
+                          makeACall(
+                            userId: userId,
+                            name: _nameController.text,
+                            context: context,
+                          );
+                        }
+                      },
+                child: const Text('Join Call'),
+              ),
+            ],
           ),
         ),
       ),
