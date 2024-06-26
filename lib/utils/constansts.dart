b798bd913e6030426d9aa6fa36c0c92c5349817e
tatarao1 <tatarao@krishworks.com>
2024-06-26 11:09:42 +0530

    updated new api key

--- lib/utils/constansts.dart
+++ lib/utils/constansts.dart
@@ -1,3 +1,4 @@
-String get apiKey => "5qy99tygpw6j";
+// String get apiKey => "5qy99tygpw6j";
+String get apiKey => "de62cgvdfbg8";
 String get getCallId => 'call-testing';
 String get apiUrl => 'https://stream-admin-chi.vercel.app';
